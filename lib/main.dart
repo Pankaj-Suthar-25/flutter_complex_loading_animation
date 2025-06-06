08fcd574e84f899e004ed39e81c6b2524637da43
Pankaj-Suthar-25 <ps25softwaredeveloper@gmail.com>
2025-06-06 08:05:13 +0530

    Loading Animation (Logo Scaling)

--- lib/main.dart
+++ lib/main.dart
@@ -14,16 +14,45 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter Complex Loading Animation'),
-        ),
-        body: const Center(
-          child: Text(
-            'Welcome to Flutter Complex Loading Animation!',
-            textAlign: TextAlign.center,
-            style: TextStyle(fontSize: 24),
-          ),
+      home: LoadingAnimation(),
+    );
+  }
+}
+
+class LoadingAnimation extends StatefulWidget {
+  const LoadingAnimation({super.key});
+
+  @override
+  State<LoadingAnimation> createState() => _LoadingAnimationState();
+}
+
+class _LoadingAnimationState extends State<LoadingAnimation>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    )..repeat(reverse: true);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Loading Animation'), centerTitle: true),
+      body: Center(
+        child: ScaleTransition(
+          scale: _controller,
+          child: const FlutterLogo(size: 100),
         ),
       ),
     );
