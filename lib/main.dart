13eab42ec5e73689c63ec76ed1e05687906b7adc
Pankaj-Suthar-25 <ps25softwaredeveloper@gmail.com>
2025-06-06 09:13:18 +0530

    Improved Loading Animation

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: RotatingLoadingAnimation(),
+      home: ImprovedLoadingAnimation(),
     );
   }
 }
@@ -97,6 +97,65 @@ class _RotatingLoadingAnimationState extends State<RotatingLoadingAnimation>
         child: RotationTransition(
           turns: _animation,
           child: const FlutterLogo(size: 100),
+        ),
+      ),
+    );
+  }
+}
+
+class ImprovedLoadingAnimation extends StatefulWidget {
+  const ImprovedLoadingAnimation({super.key});
+
+  @override
+  State<ImprovedLoadingAnimation> createState() =>
+      _ImprovedLoadingAnimationState();
+}
+
+class _ImprovedLoadingAnimationState extends State<ImprovedLoadingAnimation>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1500),
+    )..repeat(reverse: true);
+    _animation = Tween(begin: 0.0, end: 1.0).animate(_controller);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+
+    return Scaffold(
+      appBar: AppBar(
+          title: const Text('Improved Loading Animation'), centerTitle: true),
+      body: Center(
+        child: Semantics(
+          label: 'Loading animation',
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ScaleTransition(
+                scale: _animation,
+                child: RotationTransition(
+                  turns: _animation,
+                  child: FlutterLogo(size: screenSize.width * 0.2),
+                ),
+              ),
+              const SizedBox(height: 20),
+              const Text('Loading...', style: TextStyle(fontSize: 20)),
+            ],
+          ),
         ),
       ),
     );
