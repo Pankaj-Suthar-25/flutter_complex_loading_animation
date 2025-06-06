02ab485850b6392ba0e13b7ba6c60d95b21a1250
Pankaj-Suthar-25 <ps25softwaredeveloper@gmail.com>
2025-06-06 08:21:02 +0530

    Rotating Loading Animation

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: LoadingAnimation(),
+      home: RotatingLoadingAnimation(),
     );
   }
 }
@@ -52,6 +52,50 @@ class _LoadingAnimationState extends State<LoadingAnimation>
       body: Center(
         child: ScaleTransition(
           scale: _controller,
+          child: const FlutterLogo(size: 100),
+        ),
+      ),
+    );
+  }
+}
+
+class RotatingLoadingAnimation extends StatefulWidget {
+  const RotatingLoadingAnimation({super.key});
+
+  @override
+  State<RotatingLoadingAnimation> createState() =>
+      _RotatingLoadingAnimationState();
+}
+
+class _RotatingLoadingAnimationState extends State<RotatingLoadingAnimation>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1500),
+    )..repeat();
+    _animation = Tween(begin: 0.0, end: 1.0).animate(_controller);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: const Text('Rotating Loading Animation'), centerTitle: true),
+      body: Center(
+        child: RotationTransition(
+          turns: _animation,
           child: const FlutterLogo(size: 100),
         ),
       ),
